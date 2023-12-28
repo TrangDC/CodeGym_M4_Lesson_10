7cfaf3971a457717a10744fd46acf22f520546d8
TrangDC <dinhcongtrang99@gmail.com>
2023-12-28 23:25:56 +0700

    1st push

---
+++ src/main/java/com/example/springcountview/model/Counter.java
@@ -0,0 +1,24 @@
+package com.example.springcountview.model;
+
+public class Counter {
+    private int count;
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public Counter() {
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void increment() {
+        count++;
+    }
+}
